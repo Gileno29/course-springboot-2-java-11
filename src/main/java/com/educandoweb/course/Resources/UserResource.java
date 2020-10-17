658a8e569a431fff53b6e410ca1d27091d1fe4fe
Gileno cordeiro <gileno.cordeiroc@gmail.com>
2020-10-17 01:56:37 -0300

    Service layer, componet registration

--- src/main/java/com/educandoweb/course/Resources/UserResource.java
+++ src/main/java/com/educandoweb/course/Resources/UserResource.java
@@ -1,24 +1,36 @@
 package com.educandoweb.course.Resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.educandoweb.course.entities.User;
+import com.educandoweb.course.services.UserServices;
 
 @RestController //indica que é um recurso web implementado por um controlador Rest
 @RequestMapping(value="/users") //caminho do recurso
 public class UserResource {
 	
-	
+	@Autowired
+	private UserServices service;
 	@GetMapping //indica que responde a um GET do htttp
-	public ResponseEntity<User>findAll(){ //cria um metodo generico que retorna usuário
-		User u= new User(1L, "Gileno", "gileno@gmail.com","999999", "12345");
-		return ResponseEntity.ok().body(u);//retorna um ok e o conteudo desejado
+	public ResponseEntity<List<User>> findAll(){ //cria um metodo generico que retorna usuário(todos)
+		List<User> list= service.findAll();
+		return ResponseEntity.ok().body(list);//retorna um ok e o conteudo desejado
 		
 	}
 
+	@GetMapping(value="/{id}")//indica que a requisição tem um parammetro o ID
+	public ResponseEntity<User> findById(@PathVariable Long id){
+		User obj = service.findByID(id);
+		return ResponseEntity.ok().body(obj);
+		
+	}
 	
 }
 
--- src/main/java/com/educandoweb/course/services/UserServices.java
+++ src/main/java/com/educandoweb/course/services/UserServices.java
@@ -1,5 +1,30 @@
 package com.educandoweb.course.services;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.expression.spel.support.ReflectivePropertyAccessor.OptimalPropertyAccessor;
+import org.springframework.stereotype.Service;
+
+import com.educandoweb.course.entities.User;
+import com.educandoweb.course.repositories.UserRepository;
+
+@Service //Registra a classe como component do spring e permite ser injetada com o Autowired
 public class UserServices {
 	
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User>findAll(){
+		return repository.findAll();// Busca todos os USERS no BD
+	}
+	
+	public User findByID(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get(); //retorna o Objeto User do optional
+		
+	}
+		
 }
